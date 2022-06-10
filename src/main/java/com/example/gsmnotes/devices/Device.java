dfdcef0e120818f1b763d3061da6dca0f94ad99d
Georgii Semenov <george.sem@live.ru>
2022-06-10 13:43:21 +0300

    добавил id

--- src/main/java/com/example/gsmnotes/devices/Device.java
+++ src/main/java/com/example/gsmnotes/devices/Device.java
@@ -12,6 +12,8 @@ public class Device {
 
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String model;
     private String brand;
